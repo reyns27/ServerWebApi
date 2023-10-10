564892d3541de0286a9f8c95f267913495369304
Joel Reynoso <reynoso27@gmail.com>
2023-10-10 13:18:58 -0400

    setting PgSql

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Server Web Api')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('user')
+    .addTag('rol')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  const PORT = process.env.PORT || 3000;
+  //add
+  await app.listen(PORT, "0.0.0.0");
 }
 bootstrap();
